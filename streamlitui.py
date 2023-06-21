7a1f95d3e5cc9a28eb7003040ee42b0e199f6bf2
James Moura <james_moura@live.com>
2023-06-20 22:58:46 -0300

    Updates

--- streamlitui.py
+++ streamlitui.py
@@ -1,18 +1,21 @@
+# streamlit run streamlitui.py
+
 import os
 import tempfile
 import streamlit as st
 from streamlit_chat import message
 from youtubequery import YoutubeQuery
+import webbrowser
 
-st.set_page_config(page_title="Youtube to Chatbot")
-
+st.set_page_config(page_title="Youtube Vídeo Chatbot")
 
 def display_messages():
     st.subheader("Chat")
-    for i, (msg, is_user) in enumerate(st.session_state["messages"]):
+    for i, (msg, is_user, time) in enumerate(st.session_state["messages"]):
         message(msg, is_user=is_user, key=str(i))
+        if time:
+            st.write(f"Tempo no vídeo: {time}")
     st.session_state["thinking_spinner"] = st.empty()
-
 
 def process_input():
     if st.session_state["user_input"] and len(st.session_state["user_input"].strip()) > 0:
@@ -20,8 +23,8 @@ def process_input():
         with st.session_state["thinking_spinner"], st.spinner(f"Thinking"):
             query_text = st.session_state["youtubequery"].ask(user_text)
 
-        st.session_state["messages"].append((user_text, True))
-        st.session_state["messages"].append((query_text, False))
+        st.session_state["messages"].append((user_text, True, None))
+        st.session_state["messages"].append((query_text, False, None))
         
 def ingest_input():
     if st.session_state["input_url"] and len(st.session_state["input_url"].strip()) > 0:
@@ -34,38 +37,40 @@ def is_openai_api_key_set() -> bool:
 
 
 def main():
+
     if len(st.session_state) == 0:
         st.session_state["messages"] = []
         st.session_state["url"] = ""
-        st.session_state["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "")
+        st.session_state["OPENAI_API_KEY"] = "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"  # Adicione sua chave API OpenAI aqui
         if is_openai_api_key_set():
             st.session_state["youtubequery"] = YoutubeQuery(st.session_state["OPENAI_API_KEY"])
         else:
             st.session_state["youtubequery"] = None
 
-    st.header("Youtube to Chatbot")
+    st.header("🎞 Youtube Vídeo Chatbot")
 
-    if st.text_input("OpenAI API Key", value=st.session_state["OPENAI_API_KEY"], key="input_OPENAI_API_KEY", type="password"):
-        if (
-            len(st.session_state["input_OPENAI_API_KEY"]) > 0
-            and st.session_state["input_OPENAI_API_KEY"] != st.session_state["OPENAI_API_KEY"]
-        ):
-            st.session_state["OPENAI_API_KEY"] = st.session_state["input_OPENAI_API_KEY"]
-            st.session_state["messages"] = []
-            st.session_state["user_input"] = ""
-            st.session_state["input_url"] = ""
-            st.session_state["youtubequery"] = YoutubeQuery(st.session_state["OPENAI_API_KEY"])
-
-    st.subheader("Add a url")
-    st.text_input("Input url", value=st.session_state["url"], key="input_url", disabled=not is_openai_api_key_set(), on_change=ingest_input)
+    st.subheader("Adicione o url do Youtube")
+    st.text_input("Url Youtube", value=st.session_state["url"], key="input_url", disabled=not is_openai_api_key_set(), on_change=ingest_input)
 
     st.session_state["ingestion_spinner"] = st.empty()
 
+    # Verifica se o campo de entrada de URL não está vazio
+    if st.session_state["input_url"] and len(st.session_state["input_url"].strip()) > 0:
+        url = st.session_state["input_url"].strip()
+        st.video(url)
+
     display_messages()
-    st.text_input("Message", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
+    st.text_input("Faça sua pergunta", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
 
     st.divider()
-    st.markdown("Source code: [Github](https://github.com/Anil-matcha/Chat-Youtube)")
+    st.markdown("Deves Tecnologia")
+
+
+def open_video():
+    video_url = st.session_state["input_url"]
+    time = st.session_state["video_time"]
+    video_url_with_time = f"{video_url}?t={time}"
+    webbrowser.open(video_url_with_time)
 
 
 if __name__ == "__main__":
